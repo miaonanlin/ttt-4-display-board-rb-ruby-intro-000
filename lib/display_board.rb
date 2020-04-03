b49cf3b9cb42382b8f2c97c74e8826c183707306
Miao Nan Lin <miaonanlin@gmail.com>
2020-04-03 19:59:54 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = ["O", "X", "O", "O", "X", "X", "X", "O", "X"]
+def display_board(board)
+  puts"#{board[0]}|#{board[1]}|#{board[2]}"
+  puts"-----------"
+  puts"   |   |   "
+  puts"-----------"
+  puts"   |   |   "
+end
